a111e78b5a61080f41bd37fb94d4e493fb8cfe81
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-28 13:48:23 +0000

    clear out boilerplate

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
-  // Create the browser window.
+const createWindow = () => {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -9,8 +8,6 @@ function createWindow () {
       nodeIntegration: true
     }
   })
-
-  // and load the index.html of the app.
   win.loadURL('http://localhost:3000/')
 }
 
