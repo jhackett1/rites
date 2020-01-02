b520a88a824fee19f009ad03787bb2d54b2c8c48
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-02 10:01:09 +0000

    autosave works a bit better

--- src/components/EditorView/index.jsx
+++ src/components/EditorView/index.jsx
@@ -10,40 +10,41 @@ const EditorView = ({
 }) => {
 
     const [unsavedChanges, setUnsavedChanges] = useState(false)
-    const [saving, setSaving] = useState(false)
 
     const isIdle = useIdle({
-        timeToIdle: 1000
+        timeToIdle: 500
     })
 
-    // this is naive and needs improving
-    // should check:
-
-    // 1. any changes since last change
-    // 2. warn about autosave
     useEffect(() => {
-        setSaving(true)
-        isIdle && save()
-        setSaving(false)
-    }, [isIdle])
+        if (isIdle && unsavedChanges){
+            save()
+            console.log("saving...")
+            setUnsavedChanges(false)
+        }
+    }, [isIdle, save, unsavedChanges])
 
     return(
         <>
             <textarea
                 className="title-editor"
                 value={selectedDoc.title} 
-                onChange={e => handleTitleChange(e.target.value)} 
+                onChange={e => {
+                    setUnsavedChanges(true)
+                    handleTitleChange(e.target.value)
+                }} 
                 placeholder="Enter title..."
                 >
             </textarea>
             <textarea 
                 className="body-editor"
-                onChange={e => handleBodyChange(e.target.value)}
+                onChange={e => {
+                    setUnsavedChanges(true)
+                    handleBodyChange(e.target.value)
+                }}
                 placeholder="Enter body..."
                 value={selectedDoc.body}
                 >
             </textarea>
-            {saving && <p>Autosaving...</p>}
         </>
 
     )
